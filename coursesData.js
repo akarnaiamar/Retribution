865fb5e64ff5d1e95e3e952010901bfa5f971adb
akarnaiamar <rahulfirstday2025@gmail.com>
2026-04-21 11:11:48 +0600

    First Commit

---
+++ coursesData.js
@@ -0,0 +1,114 @@
+const coursesData = [
+    {
+        "name": "ACS Final Revision Batch 2025",
+        "subjects": [
+            {
+                "name": "Chemistry 1st",
+                "chapters": [
+                    {
+                        "number": 2,
+                        "title": "গুণগত রসায়ন",
+                        "videoLink": "https://youtu.be/OcZkmCBI-2Y?si=8fTEVag3oMdDNhns",
+                        "practiceSheet": "",
+                        "solutionSheet": "",
+                        "markBook": "",
+                        "otherDocuments": []
+                    },
+                    {
+                        "number": 2,
+                        "title": "গুণগত রসায়ন",
+                        "videoLink": "https://youtu.be/ddyIDpncE7M?si=HlbpqAG020ONF7gL",
+                        "practiceSheet": "",
+                        "solutionSheet": "",
+                        "markBook": "",
+                        "otherDocuments": []
+                    },
+                    {
+                        "number": 2,
+                        "title": "গুণগত রসায়ন",
+                        "videoLink": "https://youtu.be/fJy03hhPGXI?si=VZSeJRUt4Mx9fpKs",
+                        "practiceSheet": "",
+                        "solutionSheet": "",
+                        "markBook": "",
+                        "otherDocuments": []
+                    },
+                    {
+                        "number": 2,
+                        "title": "গুণগত রসায়ন",
+                        "videoLink": "https://youtu.be/KyQBKZ0375o?si=mgoKPpqt88S9MXNk",
+                        "practiceSheet": "",
+                        "solutionSheet": "",
+                        "markBook": "",
+                        "otherDocuments": []
+                    }
+                ]
+            }
+        ]
+    }
+];
+
+export default coursesData;
+
+"name": "Chemistry 2nd",
+                "chapters": [
+                    {
+                        "number": 5,
+                        "title": "",
+                        "videoLink": "",
+                        "practiceSheet": "",
+                        "solutionSheet": "",
+                        "markBook": "",
+                        "otherDocuments": []
+                    },
+                    {
+                        "number": 5,
+                        "title": "",
+                        "videoLink": "",
+                        "practiceSheet": "",
+                        "solutionSheet": "",
+                        "markBook": "",
+                        "otherDocuments": []
+                    }
+                ],     
+                "name": "Chemistry 2nd",
+                "chapters": [
+                    {
+                        "number": 5,
+                        "title": "",
+                        "videoLink": "",
+                        "practiceSheet": "",
+                        "solutionSheet": "",
+                        "markBook": "",
+                        "otherDocuments": []
+                    },
+                    {
+                        "number": 5,
+                        "title": "",
+                        "videoLink": "",
+                        "practiceSheet": "",
+                        "solutionSheet": "",
+                        "markBook": "",
+                        "otherDocuments": []
+                    }
+                ],     
+                "name": "Chemistry 2nd",
+                "chapters": [
+                    {
+                        "number": 5,
+                        "title": "",
+                        "videoLink": "",
+                        "practiceSheet": "",
+                        "solutionSheet": "",
+                        "markBook": "",
+                        "otherDocuments": []
+                    },
+                    {
+                        "number": 5,
+                        "title": "",
+                        "videoLink": "",
+                        "practiceSheet": "",
+                        "solutionSheet": "",
+                        "markBook": "",
+                        "otherDocuments": []
+                    }
+                ],  
